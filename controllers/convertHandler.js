1b4a42a7377e1f3a1cbfaf856453a55bedfc5052
winningMove <dariopapic+github@gmail.com>
2024-09-12 14:40:22 +0200

    feat: add ConvertHandler getNum functionality and unit tests

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,66 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
+  // helper to split (optional) value and unit, null if no match
+  function splitInput(input) {
+    const splitResult = input.match(/^([\d./]*)([a-zA-Z]+)$/);
+    if (splitResult) {
+      return {
+        value: splitResult[1],
+        unit: splitResult[2],
+      };
+    }
+    return null;
+  }
+  // null if multiple fractions or invalid number
+  this.getNum = function (input) {
+    const value = splitInput(input)?.value;
+
+    if (value === "") return 1;
+
+    const splitFractional = value.split("/");
+    if (splitFractional.length > 2) return null;
+
     let result;
-    
-    return result;
+    if (splitFractional.length === 1) {
+      result = parseFloat(value);
+    } else {
+      result = parseFloat(splitFractional[0]) / parseFloat(splitFractional[1]);
+    }
+
+    return !isNaN(result) ? result : null;
   };
-  
-  this.getUnit = function(input) {
+
+  this.getUnit = function (input) {
     let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
+
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
+  this.getReturnUnit = function (initUnit) {
     let result;
-    
+
     return result;
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.spellOutUnit = function (unit) {
+    let result;
+
+    return result;
+  };
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
-    
+
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
     let result;
-    
+
     return result;
   };
-  
 }
 
 module.exports = ConvertHandler;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,35 @@
-const chai = require('chai');
-let assert = chai.assert;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const { assert } = require("chai");
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 let convertHandler = new ConvertHandler();
 
-suite('Unit Tests', function(){
-
+suite("Unit Tests", function () {
+  suite("ConvertHandler", function () {
+    test("should correctly read a whole number input", () => {
+      const number = convertHandler.getNum("24km");
+      assert.isNumber(number);
+      assert.strictEqual(number, 24);
+    });
+    test("should correctly read a decimal number input", () => {
+      const decimal = convertHandler.getNum("77.6mi");
+      assert.isNumber(decimal);
+      assert.strictEqual(decimal, 77.6);
+    });
+    test("should correctly read a fractional input", () => {
+      const whole = convertHandler.getNum("9/3L");
+      assert.isNumber(whole);
+      assert.strictEqual(whole, 3);
+      const decimal = convertHandler.getNum("1/2gal");
+      assert.isNumber(decimal);
+      assert.strictEqual(decimal, 0.5);
+    });
+    test("should correctly reject a double-fraction", () => {
+      const invalid = convertHandler.getNum("1/2/3kg");
+      assert.isNull(invalid);
+    });
+    test("should correctly default to 1 on empty number value", () => {
+      const shouldBeOne = convertHandler.getNum("L");
+      assert.strictEqual(shouldBeOne, 1);
+    });
+  });
 });
