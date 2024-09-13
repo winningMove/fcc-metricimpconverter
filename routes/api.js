1b731f3a29b80d8e9bfb343abaf4698a736f89b9
winningMove <dariopapic+github@gmail.com>
2024-09-13 09:20:36 +0200

    feat: functional tests and assorted fixes

--- routes/api.js
+++ routes/api.js
@@ -6,14 +6,18 @@ const ConvertHandler = require("../controllers/convertHandler.js");
 module.exports = function (app) {
   const convertHandler = new ConvertHandler();
 
-  app.route("/api/convert", (req, res) => {
+  app.route("/api/convert").get((req, res) => {
     const input = req.query.input;
     const initNum = convertHandler.getNum(input);
     const initUnit = convertHandler.getUnit(input);
     if (initNum === null || initUnit === null) {
-      const invalidMessage = `${initNum === null ? "invalid number" : ""}${
-        initNum === null && initUnit === null ? " and " : ""
-      }${initUnit === null ? "invalid unit" : ""}`;
+      const invalidMessage = `${
+        initNum === null
+          ? initUnit === null
+            ? "invalid number and unit"
+            : "invalid number"
+          : "invalid unit"
+      }`;
       return res.json({ string: invalidMessage });
     }
     const returnNum = convertHandler.convert(initNum);
